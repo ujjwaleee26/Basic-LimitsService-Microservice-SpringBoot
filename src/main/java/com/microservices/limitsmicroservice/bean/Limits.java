0798a6ea5339229a5ec0951c47877810b2a2858b
Ujjwal Sinha <138270264+ujjwaleee26@users.noreply.github.com>
2024-01-02 07:45:40 +0530

    Update Limits.java

--- src/main/java/com/microservices/limitsmicroservice/bean/Limits.java
+++ src/main/java/com/microservices/limitsmicroservice/bean/Limits.java
@@ -16,3 +16,21 @@ public class Limits
     private int minimum;
     private int maximum;
 }
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.cloud.config.server.EnableConfigServer;
+
+// @EnableConfigServer
+// @SpringBootApplication
+// public class SpringCloudConfigServerApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+// 	}
+
+// }
+
+// spring.application.name=spring-cloud-config-server
+// server.port=8888
+// spring.cloud.config.server.git.uri=file:///Users/ujjwalsinha/git-localconfig-repo
